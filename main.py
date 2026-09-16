150e56331861483b66ce60723e9cc996b3bc860c
OiCMudkips <4535575+OiCMudkips@users.noreply.github.com>
2026-09-15 17:35:58 -0700

    add interactions handler
    it just spits the interaction back into discord

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-from flask import Flask
+from unicodedata import numeric
+import json
+from numbers import Number
+from typing import Dict, Callable
+
+from flask import Flask, json
 from flask import request
 
 app = Flask(__name__)
 
+def PingHandler(req: object) -> object:
+    return {"type": 1}
+
+def ApplicationCommandHandler(req: object) -> object:
+    return {
+        "type": 4,
+        "data": {
+            "content": f"Discord gave me this: {json.dumps(req)}",
+        }
+    }
+
+InteractionsHandlers: Dict[Number, Callable[[object], object]] = {
+    1: PingHandler,
+    2: ApplicationCommandHandler,
+}
+
 @app.post("/interactions")
 def interactions_post():
-    if request.get_json()["type"] == 1:
-        return '{"type": 1}'
+    request_body = request.get_json()
+    interaction_type = request_body["type"]
+    response = InteractionsHandlers[interaction_type](request_body)
+    return json.jsonify(response)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port="5000")
