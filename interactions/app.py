7a7918a214f761091dd6ffaf2aa3c796ffea3051
OiCMudkips <4535575+OiCMudkips@users.noreply.github.com>
2026-09-15 17:36:00 -0700

    move title to embed

--- interactions/app.py
+++ interactions/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 from discord_interactions import verify_key_decorator
 
 RIP_BOT_PUBLIC_KEY = os.getenv("RIP_BOT_PUBLIC_KEY")
-DEATH_MESSAGE_TEMPLATE = """{caption} <@{dead_person_id}>"""
+DEATH_MESSAGE_TEMPLATE = """<@{dead_person_id}>"""
 
 def convert_options_to_map(options: List) -> Dict[str, Any]:
     return {option["name"]: option["value"] for option in options}
@@ -26,8 +26,9 @@ def ApplicationCommandHandler(req: Any) -> Any:
     return {
         "type": 4,
         "data": {
-            "content": DEATH_MESSAGE_TEMPLATE.format(caption=options["caption"], dead_person_id=options["dead-person"]),
+            "content": DEATH_MESSAGE_TEMPLATE.format(dead_person_id=options["dead-person"]),
             "embeds": [{
+                "title": options["caption"],
                 "image": {
                     "url": req["data"]["resolved"]["attachments"][options["image"]]["url"],
                     "proxy_url": req["data"]["resolved"]["attachments"][options["image"]]["proxy_url"],
