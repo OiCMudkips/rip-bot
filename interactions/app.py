670f7c98490c50710b171115d85d7ad09454dfa6
OiCMudkips <4535575+OiCMudkips@users.noreply.github.com>
2026-09-15 17:36:00 -0700

    add death to databae

--- interactions/app.py
+++ interactions/app.py
@@ -1,5 +1,6 @@
-from unicodedata import numeric
-from json import dumps as json_dumps
+import json as python_json
+import sqlite3
+import time
 from numbers import Number
 from typing import Any, List, Dict, Callable
 
@@ -13,9 +14,36 @@ app = Flask(__name__)
 from discord_interactions import verify_key_decorator
 
 RIP_BOT_PUBLIC_KEY = os.getenv("RIP_BOT_PUBLIC_KEY")
+DATABASE_PATH = os.getenv("DATABASE_PATH")
 
 DEATH_MESSAGE_TEMPLATE = """<@{dead_person_id}> died!"""
 ERROR_MESSAGE = """rip-bot failed to process the command."""
+
+INSERT_DEATH_SQL = """INSERT INTO deaths VALUES (:server, :dead_person, :caption, :attachment, :timestamp, :reporter)"""
+
+def connect_to_database() -> sqlite3.Connection:
+    return sqlite3.connect(DATABASE_PATH)
+
+def insert_death(
+    cursor: sqlite3.Cursor,
+    server: str,
+    dead_person: str,
+    caption: str,
+    attachment: any,
+    timestamp: Number,
+    reporter: str,
+):
+    cursor.execute(
+        INSERT_DEATH_SQL,
+        {
+            "server": server,
+            "dead_person": dead_person,
+            "caption": caption,
+            "attachment": attachment,
+            "timestamp": timestamp,
+            "reporter": reporter,
+        },
+    )
 
 def convert_options_to_map(options: List) -> Dict[str, Any]:
     return {option["name"]: option["value"] for option in options}
@@ -25,6 +53,22 @@ def PingHandler(req: Any) -> Any:
 
 def ApplicationCommandHandler(req: Any) -> Any:
     options = convert_options_to_map(req["data"]["options"])
+    resolved_attachment = req["data"]["resolved"]["attachments"][options["image"]]
+
+    conn = connect_to_database()
+    cursor = conn.cursor()
+    insert_death(
+        cursor,
+        req["guild_id"],
+        options["dead-person"],
+        options["caption"],
+        python_json.dumps(resolved_attachment),
+        int(time.time()),
+        req["member"]["user"]["id"],
+    )
+    conn.commit()
+    conn.close()
+
     return {
         "type": 4,
         "data": {
@@ -33,7 +77,7 @@ def ApplicationCommandHandler(req: Any) -> Any:
                 {
                     "title": options["caption"],
                     "type": "image",
-                    "image": req["data"]["resolved"]["attachments"][options["image"]],
+                    "image": resolved_attachment,
                 },
             ],
         }
